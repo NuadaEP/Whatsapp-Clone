d0b45a233fe9f64ded8f375d928b73216ac3c1fe
Bruno Cardoso <bruno@liveonsolutions.com>
2018-10-02 09:21:06 -0300

    Testing resolutions

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
-import ReduxThunk from 'ReduxThunk';
+// import ReduxThunk from 'ReduxThunk';
 
 //my components
 import Route from './src/Route.js';
@@ -26,67 +26,9 @@ export default class App extends Component {
 
   render() {
     return(
-        <Provider store={ createStore(reducers, {}, applyMiddleware(ReduxThunk)) }>
+        <Provider store={ createStore(reducers) }>
           <Route />
         </Provider>
     );
   }
 }
-
-// export default App;
-
-// export default props => (
-//  <FormCadastro />
-// );
-
-//===============================================================outro
-
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow
-//  */
-
-// import React, {Component} from 'react';
-// import {Platform, StyleSheet, Text, View} from 'react-native';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-
-// type Props = {};
-// export default class App extends Component<Props> {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>Welcome to React Native!</Text>
-//         <Text style={styles.instructions}>To get started, edit App.js</Text>
-//         <Text style={styles.instructions}>{instructions}</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
