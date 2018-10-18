d2f34498ed7fea4aee05f690b2f6d583b226e755
Bruno Cardoso <bruno@liveonsolutions.com>
2018-10-17 23:22:54 -0300

    Creating a principal scene of app

--- src/Route.js
+++ src/Route.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from 'react-navigation';
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
 import BoasVindas from './components/BoasVindas';
+import Principal from './components/Principal';
 
 const RootStack = createStackNavigator(
 	{
 	  Login: FormLogin,
 	  Cadastro: FormCadastro,
-	  BoasVindas: BoasVindas
+		BoasVindas: BoasVindas,
+		Principal: Principal
 	},
 	{
 		initialRouteName: 'Login'
