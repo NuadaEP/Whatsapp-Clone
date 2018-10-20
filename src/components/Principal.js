4776cf3536dd2cd33fd2618a1066a151ca13614f
Bruno Cardoso <bruno@liveonsolutions.com>
2018-10-20 00:34:30 -0300

    separate the components that gonna be used

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { TabView, SceneMap } from 'react-native-tab-view';
+import Conversas from './Conversas';
+import Contatos from './Contatos';
 
-const Conversas = () => (
-  <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
+const _Conversas = () => (
+  // <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
+  <Conversas />
 );
-const Contatos = () => (
-  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+const _Contatos = () => (
+  // <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+  <Contatos />
 );
 
 export default class Principal extends React.Component {
@@ -25,8 +29,8 @@ export default class Principal extends React.Component {
       <TabView
         navigationState={this.state}
         renderScene={SceneMap({
-          first: Conversas,
-          second: Contatos,
+          first: _Conversas,
+          second: _Contatos,
         })}
         onIndexChange={index => this.setState({ index })}
         initialLayout={{ width: Dimensions.get('window').width }}
