e050b7cfbb15fa5356db7b3a9e69aff72fbf2adb
Bruno Cardoso <bruno@liveonsolutions.com>
2020-06-01 17:01:51 -0300

    refactory register form

--- src/Route.js
+++ src/Route.js
@@ -1,68 +1,67 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from "react-navigation";
 
-import FormLogin from './components/FormLogin';
-import FormCadastro from './components/FormCadastro';
-import BoasVindas from './components/BoasVindas';
-import Principal from './components/Principal';
-import AdicionarContato from './components/AdicionarContato';
-import Contatos from './components/Contatos';
-import Conversa from './components/Conversa';
+import FormLogin from "./components/FormLogin";
+import FormCadastro from "./components/FormCadastro";
+import BoasVindas from "./components/BoasVindas";
+import Principal from "./components/Principal";
+import AdicionarContato from "./components/AdicionarContato";
+import Conversa from "./components/Conversa";
 
 const RootStack = createStackNavigator(
-	{
-		Login: {
-			screen: FormLogin,
-			navigationOptions: () => ({
-				headerTransparent: true
-			})
-		},
+  {
+    Login: {
+      screen: FormLogin,
+      navigationOptions: () => ({
+        headerTransparent: true,
+      }),
+    },
 
-		Cadastro: {
-			screen: FormCadastro,
-			navigationOptions: () => ({
-				headerTitle: 'Cadastre-se',
-				headerStyle: {backgroundColor: '#115e54'},
-				headerTintColor: 'white',
-			})
-		},
-		
-		BoasVindas: {
-			screen: BoasVindas,
-			navigationOptions: () => ({
-				header: null
-			})
-		},
-		
-		Principal: {
-			screen: Principal,
-			navigationOptions: () => ({
-				headerTitle: 'WhatsApp Clone',
-				headerStyle: {backgroundColor: '#115e54', elevation: 0},
-				headerTintColor: 'white',
-				// headerLeft: null
-			})
-		},
+    Cadastro: {
+      screen: FormCadastro,
+      navigationOptions: () => ({
+        headerTitle: "Cadastre-se",
+        headerStyle: { backgroundColor: "#115e54" },
+        headerTintColor: "white",
+      }),
+    },
 
-		AdicionarContato: {
-			screen: AdicionarContato,
-			navigationOptions: () => ({
-				headerTitle: 'Adicionar Cotato',
-				headerStyle: {backgroundColor: '#115e54'},
-				headerTintColor: 'white',
-			})
-		},
+    BoasVindas: {
+      screen: BoasVindas,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
 
-		Conversa: {
-			screen: Conversa,
-			navigationOptions: () => ({
-				headerStyle: {backgroundColor: '#115e54'},
-				headerTintColor: 'white',
-			})
-		},
-	},
-	{
-		initialRouteName: 'Login'
-	}
+    Principal: {
+      screen: Principal,
+      navigationOptions: () => ({
+        headerTitle: "WhatsApp Clone",
+        headerStyle: { backgroundColor: "#115e54", elevation: 0 },
+        headerTintColor: "white",
+        // headerLeft: null
+      }),
+    },
+
+    AdicionarContato: {
+      screen: AdicionarContato,
+      navigationOptions: () => ({
+        headerTitle: "Adicionar Cotato",
+        headerStyle: { backgroundColor: "#115e54" },
+        headerTintColor: "white",
+      }),
+    },
+
+    Conversa: {
+      screen: Conversa,
+      navigationOptions: () => ({
+        headerStyle: { backgroundColor: "#115e54" },
+        headerTintColor: "white",
+      }),
+    },
+  },
+  {
+    initialRouteName: "Login",
+  }
 );
 
 export default RootStack;
--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,130 +1,137 @@
-import React, { Component } from 'react';
-import { 
-	View,
-	StyleSheet,
-	TextInput,
-	Button,
-	ImageBackground,
-	Text,
-	ActivityIndicator,
-	StatusBar 
-} from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  ImageBackground,
+  Text,
+  ActivityIndicator,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 
-import { modificaEmail, modificaSenha, modificaNome, cadastraUsuario } from '../actions/AutenticacaoActions';
+import {
+  modificaEmail,
+  modificaSenha,
+  modificaNome,
+  cadastraUsuario,
+} from "../actions/AutenticacaoActions";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-class FormCadastro extends Component{
+class FormCadastro extends Component {
+  _cadastraUsuario() {
+    const { nome, email, senha, navigation } = this.props;
 
-	_cadastraUsuario() {
+    this.props.cadastraUsuario({ nome, email, senha, navigation });
+  }
 
-		const { nome, email, senha, navigation } = this.props;
+  renderBtnAcessar() {
+    if (this.props.loading_login)
+      return <ActivityIndicator size="large" color="white" />;
 
-		this.props.cadastraUsuario({ nome, email, senha, navigation });
-	}
+    return (
+      <Button
+        title="Cadastrar"
+        onPress={() => this._cadastraUsuario()}
+        color="#115e54"
+      />
+    );
+  }
 
-	renderBtnAcessar(){
-		if(this.props.loading_login){
-			return( <ActivityIndicator size="large" color="white" /> )
-		}
-		return( <Button title="Cadastrar" onPress={ () => this._cadastraUsuario() } color = "#115e54" /> )
-	}
+  render() {
+    return (
+      <ImageBackground
+        style={styles.imageBackground}
+        source={require("../images/bg.png")}
+      >
+        <StatusBar backgroundColor="#114d44" />
 
-	render() {
-		return(
-			<ImageBackground style={ styles.imageBackground } source={ require('../images/bg.png') } >
+        <View style={styles.mainView}>
+          <View style={styles.formView}>
+            <TextInput
+              value={this.props.nome}
+              placeholder="Nome"
+              placeholderTextColor="#fff"
+              selectionColor="#fff"
+              style={styles.formInput}
+              returnKeyType={"next"}
+              onSubmitEditing={() => this.second.focus()}
+              onChangeText={(text) => this.props.modificaNome(text)}
+            />
 
-				<StatusBar backgroundColor="#114d44" /> 
+            <TextInput
+              value={this.props.email}
+              placeholder="E-mail"
+              placeholderTextColor="#fff"
+              selectionColor="#fff"
+              style={styles.formInput}
+              returnKeyType={"next"}
+              onSubmitEditing={() => this.third.focus()}
+              ref={(input) => (this.second = input)}
+              onChangeText={(text) => this.props.modificaEmail(text)}
+            />
 
-				<View style={ styles.mainView }>
-					<View style={ styles.formView }>
-						<TextInput 
-							value={ this.props.nome } 
-							placeholder="Nome"
-							placeholderTextColor="#fff"
-							selectionColor="#fff"
-							style={ styles.formInput } 
-							returnKeyType={ "next" }
-							onSubmitEditing={ () => this.second.focus() }
-							onChangeText={ text => this.props.modificaNome(text) } 
-						/>
+            <TextInput
+              secureTextEntry
+              value={this.props.senha}
+              placeholder="Senha"
+              placeholderTextColor="#fff"
+              selectionColor="#fff"
+              style={styles.formInput}
+              ref={(input) => (this.third = input)}
+              onChangeText={(text) => this.props.modificaSenha(text)}
+            />
 
-						<TextInput 
-							value={ this.props.email } 
-							placeholder="E-mail"
-							placeholderTextColor="#fff"
-							selectionColor="#fff"
-							style={ styles.formInput } 
-							returnKeyType={ "next" }
-							onSubmitEditing={ () => this.third.focus() }
-							ref={ input => this.second = input }
-							onChangeText={ text => this.props.modificaEmail(text) } 
-						/>
+            <Text style={styles.erro}>{this.props.erroCadastro}</Text>
+          </View>
 
-						<TextInput 
-							secureTextEntry
-							value={ this.props.senha } 
-							placeholder="Senha"
-							placeholderTextColor="#fff"
-							selectionColor="#fff"
-							style={ styles.formInput } 
-							ref={ input => this.third = input }
-							onChangeText={ text => this.props.modificaSenha(text) } 
-						/>
-
-						<Text style={ styles.erro }>{this.props.erroCadastro}</Text>
-					</View>
-
-					<View style={ styles.buttonView }>
-						{ this.renderBtnAcessar() }
-					</View>
-				</View>
-			</ImageBackground>
-			);
-	}
+          <View style={styles.buttonView}>{this.renderBtnAcessar()}</View>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-	imageBackground: {
-		flex: 1,
-		width: null
-	},
-	mainView: {
-		flex: 1,
-		padding: 10,
-	},
-	formView: {
-		flex: 4,
-		justifyContent: 'center'
-	},
-	formInput: {
-		height: 45,
-		fontSize: 20,
-		borderBottomColor: '#fff',
-		borderBottomWidth: 0.9,
-		color: '#fff'
-	},
-	buttonView: {
-		flex: 1
-	},
-	erro: {
-		color: 'red'
-	},
+  imageBackground: {
+    flex: 1,
+  },
+  mainView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  formView: {
+    marginBottom: 20,
+  },
+  formInput: {
+    width: Dimensions.get("screen").width - 50,
+    borderBottomColor: "#fff",
+    borderBottomWidth: 0.8,
+    color: "#fff",
+  },
+  buttonView: {
+    width: Dimensions.get("screen").width - 50,
+  },
+  erro: {
+    color: "red",
+  },
 });
 
-const mapStateToProps  = state => ({
-	nome: state.AutenticacaoReducer.nome,
-	email: state.AutenticacaoReducer.email,
-	senha: state.AutenticacaoReducer.senha,
-	erroCadastro: state.AutenticacaoReducer.erroCadastro,
-	sucessoCadastro: state.AutenticacaoReducer.sucessoCadastro,
-	loading_login: state.AutenticacaoReducer.loading_login
+const mapStateToProps = (state) => ({
+  nome: state.AutenticacaoReducer.nome,
+  email: state.AutenticacaoReducer.email,
+  senha: state.AutenticacaoReducer.senha,
+  erroCadastro: state.AutenticacaoReducer.erroCadastro,
+  sucessoCadastro: state.AutenticacaoReducer.sucessoCadastro,
+  loading_login: state.AutenticacaoReducer.loading_login,
 });
 
-export default connect(mapStateToProps, { 
-	modificaEmail, 
-	modificaSenha, 
-	modificaNome ,
-	cadastraUsuario
-}) (FormCadastro);
+export default connect(mapStateToProps, {
+  modificaEmail,
+  modificaSenha,
+  modificaNome,
+  cadastraUsuario,
+})(FormCadastro);
